248bb89ff897c50e97af9c8b77814448513aff4a
Hardik Sachan <sachanhardik@gmail.com>
2021-10-05 16:13:13 +0530

    Created domain of preferences page

---
+++ app/src/main/java/com/example/gocoronago/preferences/domain/entity/UserPreferences.kt
@@ -0,0 +1,10 @@
+package com.example.gocoronago.preferences.domain.entity
+
+import com.example.gocoronago.preferences.domain.constants.ColorTheme
+import com.example.gocoronago.preferences.domain.constants.Language
+
+data class UserPreferences(
+    val country: String,
+    val theme: ColorTheme,
+    val language: Language
+)
